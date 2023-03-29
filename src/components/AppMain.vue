bf8b8469f3582e3cda5190309282de19d19a261e
MonicaAuditore <mauditore@live.it>
2023-03-29 22:37:13 +0200

    init_inizializzazione progetto

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppMain",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <main></main>
+  <h1>Boolfolio front end</h1>
+</template>
+
+<style scoped></style>
