1de7ed92570521f0bb68193b60fe7d9807831f9c
MonicaAuditore <mauditore@live.it>
2023-04-01 18:22:57 +0200

    fix_postCard

---
+++ src/components/PostCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  name: "PostCard",
+  data() {
+    return {};
+  },
+  props: {
+    post: {
+      type: Object,
+    },
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="card h-100" style="width: 18rem">
+    <img
+      :src="post.img ?? 'https://placehold.co/600x400'"
+      class="card-img-top"
+      :alt="post.title"
+    />
+    <div class="card-body">
+      <h5 class="card-title">{{ post.title }}</h5>
+      <h6>
+        <i class="fa-solid fa-tag"></i>
+        {{ post.category ? post.category.name : " Nessuna categoria" }}
+      </h6>
+      <p class="card-text">
+        {{ post.content }}
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+
+    <div class="card-footer text-body-secondary">
+      <span class="badge rounded-pill text-bg-primary"> technologies </span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/pages/PostsIndex.vue
+++ src/pages/PostsIndex.vue
@@ -1,7 +1,13 @@
 <script>
 import axios from "axios";
+import PostCard from "../components/PostCard.vue";
+
 export default {
   name: "PostIndex",
+  components: {
+    PostCard,
+  },
+
   data() {
     return {
       posts: [],
@@ -48,30 +54,7 @@ export default {
 
     <div class="row mb-4 g-3">
       <div v-for="post in posts" class="col-12 col-sm-4 col-md-3">
-        <div class="card h-100" style="width: 18rem">
-          <img
-            :src="post.img ?? 'https://placehold.co/600x400'"
-            class="card-img-top"
-            :alt="post.title"
-          />
-          <div class="card-body">
-            <h5 class="card-title">{{ post.title }}</h5>
-            <h6>
-              <i class="fa-solid fa-tag"></i>
-              {{ post.category ? post.category.name : " Nessuna categoria" }}
-            </h6>
-            <p class="card-text">
-              {{ post.content }}
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
-
-          <div class="card-footer text-body-secondary">
-            <span class="badge rounded-pill text-bg-primary">
-              technologies
-            </span>
-          </div>
-        </div>
+        <PostCard :post="(post = post)" />
 
         <div
           class="card-footer text-body-secondary"
